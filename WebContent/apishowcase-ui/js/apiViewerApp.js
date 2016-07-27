1c34e49bc7da3b5a0bf5d7c8eedc5f567adc21fc
Hitesh Bargujar <hitesh.bargujar@hobsons.com>
2016-07-27 13:16:14 -0400

    changes

--- WebContent/apishowcase-ui/js/apiViewerApp.js
+++ WebContent/apishowcase-ui/js/apiViewerApp.js
@@ -1,9 +1,35 @@
 (function() {
-  var app = angular.module('apiViewer', [ 'ngRoute' ]);
+	var app = angular.module('apiViewer', [ 'ngRoute' ]);
 
-  app.controller('apiViewerCtrl', function($scope, $http){
-    
-    $http.get("http://localhost:8767/apis").success(function(response) {$scope.apis = response;});
+	app.controller('apiViewerCtrl', function($scope, $http) {
 
-  });
+		$http.get("http://localhost:8767/apis").success(function(response) {
+			$scope.apis = response;
+			console.log(response)
+		});
+
+		$scope.pingInfos = new Map();
+		$scope.gitInfos = new Map();
+
+		// ping individual service - assuming they have ping end point
+		// implemented
+		$scope.getPingInfo = function(url, id) {
+
+			$http.get(url + "/ping").success(function(response) {
+				$scope.pingInfos.set(id, response);
+			});
+		}
+
+		$scope.getGitInfo = function(gitRepoName) {
+			console.log(gitRepoName);
+
+			if (null == $scope.gitInfos.get(gitRepoName)) {
+
+				$http.get("http://localhost:8767/git/" + gitRepoName).success(function(response) {
+					$scope.gitInfos.set(gitRepoName, response);
+				});
+			}
+		}
+	});
+
 })();
